e83bd0d285dcdd96f77187355d9515e6543c954d
rahul-0402 <113822058+rahul-0402@users.noreply.github.com>
2023-01-27 11:18:11 +0530

    Refactored error response parsing

--- app/src/main/java/com/rahulghag/conduit/utils/ErrorUtils.kt
+++ app/src/main/java/com/rahulghag/conduit/utils/ErrorUtils.kt
@@ -7,50 +7,28 @@ import okhttp3.ResponseBody
 import org.json.JSONObject
 
 object ErrorUtils {
-//     fun parseErrorResponse(errorBody: ResponseBody?): UiMessage {
-//         val errorMessages = mutableListOf<String>()
-
-//         errorBody?.let {
-//             val errorResponse: JSONObject =
-//                 JSONObject(errorBody.string()).getJSONObject("errors")
-//             val keys: Iterator<String> = errorResponse.keys()
-//             while (keys.hasNext()) {
-//                 val key = keys.next()
-//                 val errorJsonArray = errorResponse.getJSONArray(key)
-//                 val typeToken = object : TypeToken<List<String>>() {}.type
-//                 val errors =
-//                     Gson().fromJson<List<String>>(errorJsonArray.toString(), typeToken)
-//                 errors.forEach { value ->
-//                     errorMessages.add("${key.capitalized()} $value")
-//                 }
-//             }
-//         }
-//         return if (errorMessages.isNotEmpty()) {
-//             UiMessage.DynamicMessage(errorMessages.joinToString("\n"))
-//         } else {
-//             UiMessage.StringResource(R.string.error_something_went_wrong)
-//         }
-//     }
-    
     fun parseErrorResponse(errorBody: ResponseBody?): UiMessage {
-        return errorBody?.let {
-            try {
-                val errorResponse = JSONObject(it.string()).getJSONObject("errors")
-                val errorMessages = errorResponse.keys().asSequence()
-                    .map { key ->
-                        val errorJsonArray = errorResponse.getJSONArray(key)
-                        Gson().fromJson<List<String>>(errorJsonArray.toString()).map { value -> "${key.capitalize()} $value" }
-                    }
-                    .flatten()
-                    .toList()
-                if (errorMessages.isNotEmpty()) {
-                    UiMessage.DynamicMessage(errorMessages.joinToString("\n"))
-                } else {
-                    UiMessage.StringResource(R.string.error_something_went_wrong)
+        val errorMessages = mutableListOf<String>()
+
+        errorBody?.let {
+            val errorResponse: JSONObject =
+                JSONObject(errorBody.string()).getJSONObject("errors")
+            val keys: Iterator<String> = errorResponse.keys()
+            while (keys.hasNext()) {
+                val key = keys.next()
+                val errorJsonArray = errorResponse.getJSONArray(key)
+                val typeToken = object : TypeToken<List<String>>() {}.type
+                val errors =
+                    Gson().fromJson<List<String>>(errorJsonArray.toString(), typeToken)
+                errors.forEach { value ->
+                    errorMessages.add("${key.capitalized()} $value")
                 }
-            } catch (e: JSONException) {
-                UiMessage.StringResource(R.string.error_something_went_wrong)
             }
-        } ?: UiMessage.StringResource(R.string.error_something_went_wrong)
+        }
+        return if (errorMessages.isNotEmpty()) {
+            UiMessage.DynamicMessage(errorMessages.joinToString("\n"))
+        } else {
+            UiMessage.StringResource(R.string.error_something_went_wrong)
+        }
     }
 }
